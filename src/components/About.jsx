ccfc8b4e0738ae473b5d61c1c1fdb3cbef7a772a
Jesus <sacj1998@gmail.com>
2025-05-03 17:52:29 -0500

    Feat: subi imagen de about y elimine las sobrantes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import styles from "./About.module.css";
 import React from "react";
-import aboutImage from '../assets/pikachiu.jpg'
+import aboutImage from '../assets/about.jpg'
 
 export default function About() {
   return (
