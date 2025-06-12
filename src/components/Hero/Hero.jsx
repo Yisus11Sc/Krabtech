f561cc126c9dd8da478f9907fcf949369187e2fd
Jesus <sacj1998@gmail.com>
2025-06-12 11:02:50 -0500

    feat: agregue accion al boton GetStarted para llevarnos a los paquetes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,42 @@
 import styles from "./Hero.module.css";
-import img from '../../assets/pcgamer2.png'
+import img from "../../assets/pcgamer2.png";
 import React from "react";
 
 export default function Hero() {
   return (
     <section id="inicio" className={styles.hero}>
-        <div className={styles.left}>
-          <h2 className={styles.titulo}>Soluciones<br/>tecnologicas<br/>para tu PC</h2>
-          <p className={styles.subtitulo}>
-            Reparaciones, limpieza y mantenimiento profesional en Cancun.
-          </p>
-          <div className={styles.botonera}>
-          <button className={styles.btn1}>Get Started</button>
-          <button 
-          onClick={() => window.open("https://wa.me/5219983785158", "_blank")}
-          className={styles.btn2}>Support Chat</button>
-          </div>
+      <div className={styles.left}>
+        <h2 className={styles.titulo}>
+          Soluciones
+          <br />
+          tecnologicas
+          <br />
+          para tu PC
+        </h2>
+        <p className={styles.subtitulo}>
+          Reparaciones, limpieza y mantenimiento profesional en Cancun.
+        </p>
+        <div className={styles.botonera}>
+          <button
+            onClick={() => {
+              const section = document.getElementById("paquetes");
+              section?.scrollIntoView({ behavior: "smooth" });
+            }}
+            className={styles.btn1}
+          >
+            Get Started
+          </button>
+          <button
+            onClick={() => window.open("https://wa.me/5219983785158", "_blank")}
+            className={styles.btn2}
+          >
+            Support Chat
+          </button>
         </div>
-        <div className={styles.right}>
-            <img src={img} alt="pc-gamer" />
-        </div>
+      </div>
+      <div className={styles.right}>
+        <img src={img} alt="pc-gamer" />
+      </div>
     </section>
   );
 }
